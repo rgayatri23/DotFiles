a9b8090368def49f6c2f36d0d101bcf6151ed1e1
Rahulkumar Gayatri <rahulgayatri84@gmail.com>
2024-05-28 18:00:51 -0700

    Add nerdcommenter and markdownpreview

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -18,6 +18,8 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-tree/nvim-web-devicons'
+  use {'iamcco/markdown-preview.nvim'}
+  use 'preservim/nerdcommenter'
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
